3c0019696667956064d615803e4f0ea8d3838e16
Luis Felipe <silva.luisfelipe13@gmail.com>
2018-07-16 15:38:00 -0300

    Upd install script

--- install.bash
+++ install.bash
@@ -21,24 +21,13 @@ sudo apt install terminator vim clang-format-5.0 build-essential cmake curl zsh 
 sudo apt install git git-core git-gui gitk -y
 sudo apt install python-dev python-pip python3-dev python3-pi -y
 sudo apt install python3-numpy python3-matplotlib python-numpy python-matplotlib -y
-curk -sSL https://get.docker.com | sh
+curl -sSL https://get.docker.com | sh
 
 # beyond stuffs
-sudo apt install libboost-dev libmosquitto-dev mosquitto-dev mosquitto-clients libmosquitto-dev libcurl4-gnutls-dev libjson-c-dev \
-    libboost-system-dev libboost-all-dev libncurses5-dev zlib1g-dev gawk libssl-dev libssl1.0-dev subversion \
-    libjson-c-dev libcurl4-openssl-dev -y
-
-# Plug Vim
-sudo apt install exuberant-ctags -y && curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# labima stuffs
-
-if [ ! -d ".oh-my-zsh" ]; then
-    echo "Intalling oh-my-zsh"
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-else
-    echo "oh-my-zsh already installed: skipping"
-fi
+sudo apt install libboost-dev libmosquitto-dev mosquitto-dev mosquitto-clients libmosquitto-dev \
+    libcurl4-gnutls-dev libjson-c-dev libboost-system-dev libboost-all-dev libncurses5-dev \
+    zlib1g-dev gawk libssl-dev libssl1.0-dev subversion libjson-c-dev curl libcurl4-openssl-dev \
+    sshpass -y
 
 if [ ! -d "beyond" ]; then
     echo "Creating beyond"
@@ -47,12 +36,28 @@ else
     echo "beyond already created: skipping"
 fi
 
+# labima stuffs
 if [ ! -d "labima" ]; then
     echo "Creating labima"
     mkdir labima
 else
     echo "labima already created: skipping"
 fi
+
+
+
+if [ ! -d ".oh-my-zsh" ]; then
+    echo "Intalling oh-my-zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+    echo "oh-my-zsh already installed: skipping"
+fi
+
+
+# Plug Vim
+sudo apt install exuberant-ctags -y && curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 
 echo -e "${GREEN}Configin git ... ${NC}\n"
 git config --global user.email "silva.luisfelipe13@gmail.com"
