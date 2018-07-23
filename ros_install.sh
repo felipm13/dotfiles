74eaebf0af869f553d1f82b1db93f5c5ab7f9ae2
Luis Felipe <silva.luisfelipe13@gmail.com>
2018-07-23 12:41:29 -0300

    Added ros install script

---
+++ ros_install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+sudo apt-get update
+
+sudo apt-get install ros-melodic-desktop
+
+sudo rosdep init
+rosdep update
+
+echo "source /opt/ros/melodic/setup.zsh" >> ~/.zshrc
+source ~/.zshrc
+
+sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
+
