7eae7045100b63378f285f6713db3008984cb49f
Luis Felipe <silva.luisfelipe13@gmail.com>
2018-12-14 21:17:15 -0200

    Add google chrome

--- install.bash
+++ install.bash
@@ -15,6 +15,10 @@ cd ~
 
 sudo apt update
 
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+rm google-chrome-stable_current_amd64.deb
+
 sudo apt install \
     terminator \
     vim \
